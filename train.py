45c25d091f3da4ae96f1d3d6b5d447aab8605e64
Ajith Thomas <ajiththomas152@gmail.com>
2019-04-05 13:03:30 +0530

    Added cost per sequence and momentum

--- train.py
+++ train.py
@@ -9,10 +9,13 @@ from ntm import NTM
 from seqgen import generate_patterns
 
 # Train parameters
-epoch = 100
-batch_size = 1
-steps_per_epoch = 100*100
+epochs = 100
+batch_size = 5
+steps_per_epoch = 10000
 learning_rate = 1e-4
+momentum = 0.9
+clip_grad_min = -10.0
+clip_grad_max = 10.0
 
 # NTM parameters
 controller_size = 100
@@ -28,18 +31,16 @@ out_bits = 8
 
 # Training
 ntm_model = NTM(controller_size, memory_locations, memory_vector_size, maximum_shifts, out_bits)
-optimizer = tf.optimizers.RMSprop(learning_rate=learning_rate)
+optimizer = tf.optimizers.RMSprop(learning_rate=learning_rate, momentum=momentum)
 bce_loss = tf.losses.BinaryCrossentropy()
 
-clip_grad_min = -10.0
-clip_grad_max = 10.0
-
+# Stateful metrics
 train_loss = tf.metrics.Mean(name="train_loss")
-train_accuracy = tf.metrics.BinaryAccuracy(name="train_accuracy")
+train_cost = tf.metrics.Mean(name="train_cost")
 
 # Checkpoints
 ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=ntm_model)
-manager = tf.train.CheckpointManager(ckpt, './ntm_ckpt', max_to_keep=3)
+manager = tf.train.CheckpointManager(ckpt, './tf_ntm_ckpt', max_to_keep=3)
 ckpt.restore(manager.latest_checkpoint)
 
 if manager.latest_checkpoint:
@@ -48,22 +49,29 @@ else:
     print("Training NTM model from scratch")
 
 
+def cost_per_sequence(y_true, y_pred):  # Calculates the bit errors per sequence
+    cost = tf.reduce_sum(tf.abs(y_true - y_pred))
+    cost = cost/tf.cast(y_true.shape[0], tf.float32)
+    return cost
+
+
 def train_one_step(x, y):
     with tf.GradientTape() as tape:
         y_pred = ntm_model(x, training=True)
         loss = bce_loss(y, y_pred)
+        cost = cost_per_sequence(y, y_pred)
 
     gradients = tape.gradient(loss, ntm_model.trainable_variables)
     clipped_gradients = [tf.clip_by_value(grads, clip_grad_min, clip_grad_max) for grads in gradients]
     optimizer.apply_gradients(zip(clipped_gradients, ntm_model.trainable_variables))
 
     train_loss(loss)
-    train_accuracy(y, y_pred)
+    train_cost(cost)
 
 
 # Training loop
 try:
-    for e in range(epoch):
+    for epoch in range(epochs):
         for step in range(steps_per_epoch):
             x, y = generate_patterns(batch_size, max_sequence, min_sequence, in_bits, out_bits,
                                      pad=K.epsilon(), low_tol=K.epsilon())
@@ -72,10 +80,10 @@ try:
 
             template = "Epoch: {}, Train step: {}, Train loss: {}, Train accuracy: {}"
             print(template.format(
-                                e+1,
+                                epoch+1,
                                 step+1,
                                 train_loss.result(),
-                                train_accuracy.result()*100.0
+                                train_cost.result()
                                 ))
 
             ckpt.step.assign_add(1)
@@ -83,20 +91,23 @@ try:
                 save_path = manager.save()
                 print("Saved checkpoint for step {}: {}".format(int(ckpt.step), save_path))
 
+        train_loss.reset_states()
+        train_cost.reset_states()
+
 except KeyboardInterrupt:
     print("User interrupted")
 
 # Visualize the prediction made by the model
-x, y = generate_patterns(batch_size, max_sequence, min_sequence, in_bits, out_bits, fixed_seq_len=False)
+x, y = generate_patterns(batch_size, max_sequence, min_sequence, in_bits, out_bits, fixed_seq_len=True)
 y_pred = ntm_model(x)
 rt, r_wt, at, w_wt, Mt = ntm_model.debug_ntm()
 
-plt.matshow(rt)
-plt.matshow(at)
-plt.matshow(w_wt)
-plt.matshow(r_wt)
-plt.matshow(Mt)
+plt.matshow(rt, cmap=plt.get_cmap('jet'))
+plt.matshow(at, cmap=plt.get_cmap('jet'))
+plt.matshow(w_wt, cmap=plt.get_cmap('gray'))
+plt.matshow(r_wt, cmap=plt.get_cmap('gray'))
+plt.matshow(Mt, cmap=plt.get_cmap('jet'))
 
-plt.matshow(x[0])
-plt.matshow(y_pred[0])
+plt.matshow(x[0], cmap=plt.get_cmap('jet'))
+plt.matshow(y_pred[0], cmap=plt.get_cmap('jet'))
 plt.show()
